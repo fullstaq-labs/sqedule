8f4c03e018e51edbb28da9e7ac362afc2969d9f8
Hongli Lai <h.lai@fullstaq.com>
2021-06-22 11:18:16 +0200

    Avoid hitting max database connections during tests
    Reuse existing database connections.

--- server/dbutils/test_support.go
+++ server/dbutils/test_support.go
@@ -3,21 +3,17 @@ package dbutils
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 )
 
-// SetupTestDatabase ...
+var cachedDbconn *gorm.DB
+var cachedDbconnMutex sync.Mutex
+
 func SetupTestDatabase() (*gorm.DB, error) {
-	logger := gormlogger.Default.LogMode(gormlogger.Warn)
-	db, err := EstablishDatabaseConnection(
-		// TODO: make database parameters configurable
-		"postgresql",
-		"dbname=sqedule_test",
-		&gorm.Config{
-			Logger: logger,
-		})
+	db, err := getCachedOrNewDbconn()
 	if err != nil {
 		return nil, fmt.Errorf("Error establishing database connection: %w", err)
 	}
@@ -25,6 +21,32 @@ func SetupTestDatabase() (*gorm.DB, error) {
 	err = ClearDatabase(context.Background(), db)
 	if err != nil {
 		return nil, fmt.Errorf("Error clearing database: %w", err)
+	}
+
+	return db, nil
+}
+
+func getCachedOrNewDbconn() (*gorm.DB, error) {
+	cachedDbconnMutex.Lock()
+	defer cachedDbconnMutex.Unlock()
+
+	var db *gorm.DB = cachedDbconn
+	var err error
+
+	if db == nil {
+		logger := gormlogger.Default.LogMode(gormlogger.Warn)
+		db, err = EstablishDatabaseConnection(
+			// TODO: make database parameters configurable
+			"postgresql",
+			"dbname=sqedule_test",
+			&gorm.Config{
+				Logger: logger,
+			})
+		if err != nil {
+			return nil, err
+		}
+
+		cachedDbconn = db
 	}
 
 	return db, nil
