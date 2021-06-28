47a5ec9fec8548ae1e2fde1cb4d94de6cc66725a
Hongli Lai <h.lai@fullstaq.com>
2021-06-28 07:55:58 +0200

    CLI for reviewing and deleting approval ruleset proposals

---
+++ cmd/sqedule-cli/approval_ruleset_proposal_delete.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	encjson "encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/fullstaq-labs/sqedule/cli"
+	"github.com/fullstaq-labs/sqedule/lib/mocking"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// approvalRulesetProposalDeleteCmd represents the 'approval-ruleset proposal delete' command
+var approvalRulesetProposalDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete an approval ruleset proposal",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		viper.BindPFlags(cmd.Flags())
+		return approvalRulesetProposalDeleteCmd_run(viper.GetViper(), mocking.RealPrinter{}, false)
+	},
+}
+
+func approvalRulesetProposalDeleteCmd_run(viper *viper.Viper, printer mocking.IPrinter, testing bool) error {
+	err := approvalRulesetProposalDeleteCmd_checkConfig(viper)
+	if err != nil {
+		return err
+	}
+
+	config := cli.LoadConfigFromViper(viper)
+	state, err := cli.LoadStateFromFilesystem()
+	if err != nil {
+		return fmt.Errorf("Error loading state: %w", err)
+	}
+
+	req, err := cli.NewApiRequest(config, state)
+	if err != nil {
+		return err
+	}
+
+	var result interface{}
+	resp, err := req.
+		SetResult(&result).
+		Delete(fmt.Sprintf("/approval-rulesets/%s/proposals/%s",
+			url.PathEscape(viper.GetString("approval-ruleset-id")),
+			url.PathEscape(viper.GetString("id"))))
+	if err != nil {
+		return err
+	}
+	if resp.IsError() {
+		return fmt.Errorf("Error deleting approval ruleset proposal: %s", cli.GetApiErrorMessage(resp))
+	}
+
+	output, err := encjson.MarshalIndent(result, "", "    ")
+	if err != nil {
+		return fmt.Errorf("Error formatting result as JSON: %w", err)
+	}
+	printer.Println(string(output))
+
+	return nil
+}
+
+func approvalRulesetProposalDeleteCmd_checkConfig(viper *viper.Viper) error {
+	return cli.RequireConfigOptions(viper, cli.ConfigRequirementSpec{
+		StringNonEmpty: []string{"approval-ruleset-id", "id"},
+	})
+}
+
+func init() {
+	cmd := approvalRulesetProposalDeleteCmd
+	flags := cmd.Flags()
+	approvalRulesetProposalCmd.AddCommand(cmd)
+
+	cli.DefineServerFlags(flags)
+
+	flags.String("approval-ruleset-id", "", "")
+	flags.String("id", "", "")
+}
---
+++ cmd/sqedule-cli/approval_ruleset_proposal_review.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	encjson "encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/fullstaq-labs/sqedule/cli"
+	"github.com/fullstaq-labs/sqedule/lib/mocking"
+	"github.com/fullstaq-labs/sqedule/server/httpapi/json"
+	"github.com/fullstaq-labs/sqedule/server/httpapi/json/reviewstate"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// approvalRulesetProposalReviewCmd represents the 'approval-ruleset proposal review' command
+var approvalRulesetProposalReviewCmd = &cobra.Command{
+	Use:   "review",
+	Short: "Review an approval ruleset proposal",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		viper.BindPFlags(cmd.Flags())
+		return approvalRulesetProposalReviewCmd_run(viper.GetViper(), mocking.RealPrinter{}, false)
+	},
+}
+
+func approvalRulesetProposalReviewCmd_run(viper *viper.Viper, printer mocking.IPrinter, testing bool) error {
+	err := approvalRulesetProposalReviewCmd_checkConfig(viper)
+	if err != nil {
+		return err
+	}
+
+	config := cli.LoadConfigFromViper(viper)
+	state, err := cli.LoadStateFromFilesystem()
+	if err != nil {
+		return fmt.Errorf("Error loading state: %w", err)
+	}
+
+	req, err := cli.NewApiRequest(config, state)
+	if err != nil {
+		return err
+	}
+
+	var result interface{}
+	resp, err := req.
+		SetBody(approvalRulesetProposalReviewCmd_createBody(viper)).
+		SetResult(&result).
+		Put(fmt.Sprintf("/approval-rulesets/%s/proposals/%s/review-state",
+			url.PathEscape(viper.GetString("approval-ruleset-id")),
+			url.PathEscape(viper.GetString("id"))))
+	if err != nil {
+		return err
+	}
+	if resp.IsError() {
+		return fmt.Errorf("Error reviewing approval ruleset proposal: %s", cli.GetApiErrorMessage(resp))
+	}
+
+	output, err := encjson.MarshalIndent(result, "", "    ")
+	if err != nil {
+		return fmt.Errorf("Error formatting result as JSON: %w", err)
+	}
+	printer.Println(string(output))
+
+	return nil
+}
+
+func approvalRulesetProposalReviewCmd_checkConfig(viper *viper.Viper) error {
+	return cli.RequireConfigOptions(viper, cli.ConfigRequirementSpec{
+		StringNonEmpty: []string{"approval-ruleset-id", "id", "action"},
+	})
+}
+
+func approvalRulesetProposalReviewCmd_createBody(viper *viper.Viper) json.ReviewableReviewStateInput {
+	var state reviewstate.Input
+
+	switch viper.GetString("action") {
+	case "approve":
+		state = reviewstate.Approved
+	case "reject":
+		state = reviewstate.Rejected
+	default:
+		panic("Unsupported action parameter '" + viper.GetString("action") + "'")
+	}
+
+	return json.ReviewableReviewStateInput{
+		State: state,
+	}
+}
+
+func init() {
+	cmd := approvalRulesetProposalReviewCmd
+	flags := cmd.Flags()
+	approvalRulesetProposalCmd.AddCommand(cmd)
+
+	cli.DefineServerFlags(flags)
+
+	flags.String("approval-ruleset-id", "", "")
+	flags.String("id", "", "")
+	flags.String("action", "", "'approve' or 'reject'")
+}
