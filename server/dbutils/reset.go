4bfbc8c4d6c97e8c455c03709428ac608a44bf7a
Hongli Lai <h.lai@fullstaq.com>
2021-07-04 11:42:29 +0200

    Make resetting the database (as part of tests faster)
    Controller tests go from 103s to 15s.

--- server/dbutils/reset.go
+++ server/dbutils/reset.go
@@ -3,13 +3,15 @@ package dbutils
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"gorm.io/gorm"
 )
 
 func listTableNames(db *gorm.DB) ([]string, error) {
 	return QueryStringList(db, "SELECT table_name FROM information_schema.tables"+
-		" WHERE table_schema = current_schema() AND table_type = 'BASE TABLE'")
+		" WHERE table_schema = current_schema() AND table_type = 'BASE TABLE'"+
+		" ORDER BY table_name")
 }
 
 func listUserDefinedTypes(db *gorm.DB) ([]string, error) {
@@ -19,11 +21,12 @@ func listUserDefinedTypes(db *gorm.DB) ([]string, error) {
 		" WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))"+
 		" AND n.nspname = current_schema()"+
 		" AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)"+
-		" AND t.typname != 'citext'")
+		" AND t.typname != 'citext'"+
+		" ORDER BY type")
 }
 
 func listExtensions(db *gorm.DB) ([]string, error) {
-	return QueryStringList(db, "SELECT extname FROM pg_extension WHERE extname != 'plpgsql'")
+	return QueryStringList(db, "SELECT extname FROM pg_extension WHERE extname != 'plpgsql' ORDER BY extname")
 }
 
 // ClearDatabase clears all tables and sequences in the current database.
@@ -35,11 +38,9 @@ func ClearDatabase(context context.Context, db *gorm.DB) error {
 		}
 		db.Logger.Info(context, "List of tables: %v", tableNames)
 
-		for _, tableName := range tableNames {
-			err = db.Exec(fmt.Sprintf("TRUNCATE TABLE %s CASCADE", tableName)).Error
-			if err != nil {
-				return fmt.Errorf("error truncating table %s: %w", tableName, err)
-			}
+		err = tx.Exec(fmt.Sprintf("TRUNCATE TABLE %s RESTART IDENTITY CASCADE", strings.Join(tableNames, ", "))).Error
+		if err != nil {
+			return fmt.Errorf("error truncating database tables: %w", err)
 		}
 
 		return nil
