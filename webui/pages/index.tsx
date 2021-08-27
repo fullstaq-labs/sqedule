696d97ed84b53e11c9591e8acc714d2ad81305ae
Hongli Lai <h.lai@fullstaq.com>
2021-08-27 18:20:03 +0200

    Remove unused code

--- webui/pages/index.tsx
+++ webui/pages/index.tsx
@@ -3,7 +3,6 @@ import { IAppContext } from '../components/app_context';
 import { NavigationSection } from '../components/navbar';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import MaterialLink from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
@@ -34,27 +33,6 @@ export default function DashboardPage(_props: IProps) {
           </Typography>
         </Box>
       </Container>
-      {/* <Box mx={2} my={2} style={{ display: 'flex', flexGrow: 1 }}>
-        <Paper style={{ flexGrow: 1 }}>
-          <Box mx={2}>
-            <Typography variant="body1">
-              <p>
-                <MaterialLink href="https://github.com/fullstaq-labs/sqedule" target="_blank" rel="noopener">
-                  <Image src="/logo-sqedule-horizontal.svg" alt="Sqedule logo" height={150} width={508} />
-                </MaterialLink>
-              </p>
-              <p>
-                Welcome to Sqedule, the application release &amp; auditing platform.
-              </p>
-              <p>
-                <MaterialLink component={LinkButton} href="https://docs.sqedule.io/user_guide/" target="_blank" rel="noopener">
-                  Read the docs
-                </MaterialLink>
-              </p>
-            </Typography>
-          </Box>
-        </Paper>
-      </Box> */}
     </>
   );
 }
